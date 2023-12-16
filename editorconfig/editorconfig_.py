d2c00fd6872575e4f93a83ad35a3395ce49a89b7
Filip Szymański <fszymanski.pl@gmail.com>
2023-12-16 14:12:18 +0100

    editorconfig: Use contextlib.suppress to handle ValueError

--- editorconfig/editorconfig_.py
+++ editorconfig/editorconfig_.py
@@ -1,4 +1,4 @@
-# Copyright (C) 2021 Filip Szymański <fszymanski.pl@gmail.com>
+# Copyright (C) 2023 Filip Szymański <fszymanski.pl@gmail.com>
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,16 +14,18 @@
 # along with this program. If not, see <https://www.gnu.org/licenses/>.
 #
 
+from contextlib import suppress
+
 import gi
 
-gi.require_version('Pluma', '1.0')
+gi.require_version("Pluma", "1.0")
 from gi.repository import Gio, GObject, Pluma
 
 import editorconfig
 
 
 class EditorConfigPlugin(GObject.Object, Pluma.ViewActivatable):
-    __gtype_name__ = 'EditorConfigPlugin'
+    __gtype_name__ = "EditorConfigPlugin"
 
     view = GObject.Property(type=Pluma.View)
 
@@ -32,18 +34,18 @@ class EditorConfigPlugin(GObject.Object, Pluma.ViewActivatable):
 
     def do_activate(self):
         doc = self.view.get_buffer()
-        if not hasattr(doc, 'editorconfig_handler'):
-            doc.editorconfig_handler = doc.connect('loaded', lambda d, _: self.apply_editorconfig(d))
+        if not hasattr(doc, "editorconfig_handler"):
+            doc.editorconfig_handler = doc.connect("loaded", lambda d, _: self.apply_editorconfig(d))
 
     def do_deactivate(self):
         app = Pluma.App.get_default()
         window = app.get_active_window()
         for doc in window.get_documents():
-            if hasattr(doc, 'editorconfig_handler'):
+            if hasattr(doc, "editorconfig_handler"):
                 doc.disconnect(doc.editorconfig_handler)
                 del doc.editorconfig_handler
 
-            if hasattr(doc, 'editorconfig_trim_trailing_whitespace_handler'):
+            if hasattr(doc, "editorconfig_trim_trailing_whitespace_handler"):
                 doc.disconnect(doc.editorconfig_trim_trailing_whitespace_handler)
                 del doc.editorconfig_trim_trailing_whitespace_handler
 
@@ -51,49 +53,45 @@ class EditorConfigPlugin(GObject.Object, Pluma.ViewActivatable):
         pass
 
     def parse_editorconfig(self, doc):
-        location = doc.get_location()
-        if location is not None and location.query_exists():
+        if (location := doc.get_location()) is not None and location.query_exists():
             try:
                 return editorconfig.get_properties(location.get_path())
             except editorconfig.EditorConfigError:
                 pass
 
     def apply_editorconfig(self, doc):
-        config = self.parse_editorconfig(doc)
-        if config is None:
+        if (conf := self.parse_editorconfig(doc)) is None:
             return
 
-        for name, value in config.items():
-            if name == 'indent_style':
-                if value == 'tab':
+        for name, value in conf.items():
+            if name == "indent_style":
+                if value == "tab":
                     self.view.set_insert_spaces_instead_of_tabs(False)
-                elif value == 'space':
+                elif value == "space":
                     self.view.set_insert_spaces_instead_of_tabs(True)
-            elif name == 'indent_size':
-                if value == 'tab' and 'tab_width' in config:
-                    self.view.set_tab_width(int(config['tab_width']))
+            elif name == "indent_size":
+                if value == "tab" and "tab_width" in conf:
+                    with suppress(ValueError): self.view.set_tab_width(int(conf["tab_width"]))
                 else:
-                    try:
-                        self.view.set_tab_width(int(value))
-                    except ValueError:
-                        pass
-            elif name == 'tab_width':
-                self.view.set_tab_width(int(value))
-            elif name == 'end_of_line':
-                if value == 'lf':
-                    doc.set_property('newline-type', Pluma.DocumentNewlineType.LF)
-                elif value == 'cr':
-                    doc.set_property('newline-type', Pluma.DocumentNewlineType.CR)
-                elif value == 'crlf':
-                    doc.set_property('newline-type', Pluma.DocumentNewlineType.CR_LF)
-            elif name == 'trim_trailing_whitespace':
-                settings = Gio.Settings.new('org.mate.pluma')
-                if 'trailsave' not in settings.get_value('active-plugins'):
-                    if not hasattr(doc, 'editorconfig_trim_trailing_whitespace_handler'):
+                    with suppress(ValueError): self.view.set_tab_width(int(value))
+            elif name == "tab_width":
+                with suppress(ValueError): self.view.set_tab_width(int(value))
+            elif name == "end_of_line":
+                if value == "lf":
+                    doc.set_property("newline-type", Pluma.DocumentNewlineType.LF)
+                elif value == "cr":
+                    doc.set_property("newline-type", Pluma.DocumentNewlineType.CR)
+                elif value == "crlf":
+                    doc.set_property("newline-type", Pluma.DocumentNewlineType.CR_LF)
+            elif name == "trim_trailing_whitespace":
+                settings = Gio.Settings("org.mate.pluma")
+                if "trailsave" not in settings.get_value("active-plugins"):
+                    if not hasattr(doc, "editorconfig_trim_trailing_whitespace_handler"):
                         doc.editorconfig_trim_trailing_whitespace_handler = doc.connect(
-                            'save', lambda d, *_: self.trim_trailing_whitespace(d))
-            elif name == 'max_line_length':
-                self.view.set_right_margin_position(int(value))
+                            "save", lambda d, *_: self.trim_trailing_whitespace(d))
+            elif name == "max_line_length":
+                self.view.set_property("show-right-margin", True)
+                with suppress(ValueError): self.view.set_right_margin_position(int(value))
 
     def trim_trailing_whitespace(self, doc):
         for linenr in range(0, doc.get_line_count()):
@@ -108,5 +106,3 @@ class EditorConfigPlugin(GObject.Object, Pluma.ViewActivatable):
 
             if not start.equal(end):
                 doc.delete(start, end)
-
-# vim: ft=python3 ts=4 et
